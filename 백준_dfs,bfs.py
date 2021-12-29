d8873bae2f619c42df1e515deb5c1518db3fe0fd
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2021-12-29 09:29:30 +0900

    no message

--- 백준_dfs,bfs.py
+++ 백준_dfs,bfs.py
@@ -193,3 +193,47 @@ def bfs(a,c):
     return c
 
 print(bfs(a,-1))
+
+# 7569
+
+from collections import deque
+
+m,n,h=map(int, input().split())
+a=[]
+b=[]
+for _ in range(h):
+    for _ in range(n):
+       b.append(list(map(int, input().split())))
+    a.append(b)
+    b=[]
+
+def bfs(a,c):
+    dx=[1,-1,0,0,0,0]
+    dy=[0,0,1,-1,0,0]
+    dz=[0,0,0,0,1,-1]
+    q=deque()
+    for i in range(h):
+        for j in range(n):
+            for k in range(m):
+                if a[i][j][k] ==1:
+                    q.append([i,j,k])    
+    while q:
+        c += 1                           
+        for _ in range(len(q)):
+            z,x,y=q.popleft()
+            for i in range(6):
+                nx=x+dx[i]
+                ny=y+dy[i]
+                nz=z+dz[i]
+                if -1<nx<n and -1<ny<m and -1<nz<h:
+                    if a[nz][nx][ny] == 0:
+                        a[nz][nx][ny] = 1
+                        q.append([nz,nx,ny])
+    for i in a:
+        for j in i:
+            if 0 in j:
+                return -1
+    
+    return c-1
+
+print(bfs(a,0))
