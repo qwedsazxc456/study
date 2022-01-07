de22b0e8a23b2737479c9df4c949c73ece87bc1c
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2022-01-07 22:32:16 +0900

    no message

--- 푸는중.py
+++ 푸는중.py
@@ -1,15 +1,1 @@
-from collections import deque
-t=int(input())
-for _ in range(t):
-    v,e=map(int, input().split())
-    g=[[] for _ in range(v+1)]
-    bi=[[] for _ in range(v+1)]
-    for _ in range(e):
-        a,b=map(int, input().split())
-        g[a].append(b)
-        g[b].append(a)
-def bfs(s):
-    bi[s]=1
-    
-    
-    
+ 
