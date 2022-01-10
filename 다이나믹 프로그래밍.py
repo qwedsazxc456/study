3e2f81ce0b0206e6dbb32cdf739b837bdc9cb356
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2022-01-11 01:23:19 +0900

    no message

--- 다이나믹 프로그래밍.py
+++ 다이나믹 프로그래밍.py
@@ -122,3 +122,15 @@ for _ in range(t):
     n = int(input())
     print(a[n-1])
     
+# 1149
+
+n = int(input())
+a=[]
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(1,n):
+    a[i][0]+=min(a[i-1][1],a[i-1][2])
+    a[i][1]+=min(a[i-1][0],a[i-1][2])
+    a[i][2]+=min(a[i-1][1],a[i-1][0])
+print(min(a[n-1]))
+
--- 푸는중.py
+++ 푸는중.py
@@ -1,7 +1,9 @@
-a=[1,1,1,2,2]+[0 for _ in range(95)]
-t=int(input())
-for i in range(95):
-    a[i+5] = a[i]+a[i+4]
-for _ in range(t):
-    n = int(input())
-    print(a[n-1])
+n = int(input())
+a=[]
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(1,n):
+    a[i][0]+=min(a[i-1][1],a[i-1][2])
+    a[i][1]+=min(a[i-1][0],a[i-1][2])
+    a[i][2]+=min(a[i-1][1],a[i-1][0])
+print(min(a[n-1]))
