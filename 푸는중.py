80f0437037e15706b17c3570f90522b594817ee3
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2022-01-11 01:23:36 +0900

    no message

--- 푸는중.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-a=[]
-for _ in range(n):
-    a.append(list(map(int, input().split())))
-for i in range(1,n):
-    a[i][0]+=min(a[i-1][1],a[i-1][2])
-    a[i][1]+=min(a[i-1][0],a[i-1][2])
-    a[i][2]+=min(a[i-1][1],a[i-1][0])
-print(min(a[n-1]))
