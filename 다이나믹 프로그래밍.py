f454cccd32e667a60c8ab66d6e87be9fa6e90c4b
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2022-01-12 15:17:54 +0900

    no message

--- 다이나믹 프로그래밍.py
+++ 다이나믹 프로그래밍.py
@@ -134,3 +134,23 @@ for i in range(1,n):
     a[i][2]+=min(a[i-1][1],a[i-1][0])
 print(min(a[n-1]))
 
+# 1932
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+a=[]
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    for j in range(len(a[i])):
+        if j == 0:
+            a[i][j] += a[i-1][j]
+        elif j == len(a[i])-1:
+            a[i][j] += a[i-1][j-1]
+        else:
+            a[i][j] += max(a[i-1][j-1],a[i-1][j])
+print(max(a[n-1]))
+
