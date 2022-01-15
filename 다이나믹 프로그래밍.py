bfe0b9ef7dcffb9f48d6c330040a2e4d34ee2fb4
DESKTOP-7HEDPNB\user <qwedsazxc456@naver.com>
2022-01-15 14:40:22 +0900

    no message

--- 다이나믹 프로그래밍.py
+++ 다이나믹 프로그래밍.py
@@ -196,3 +196,19 @@ if n != 1:
         c += 1
 
 print(a[n])
+
+# 10844
+
+n = int(input())
+a=[[0]*10 for _ in range(101)]
+for i in range(1,10):
+    a[1][i]=1
+for i in range(2,n+1):
+    for j in range(10):
+        if j == 0:
+            a[i][0] = a[i-1][1]
+        elif j == 9:
+            a[i][9] = a[i-1][8]
+        else:
+            a[i][j] = a[i-1][j-1]+a[i-1][j+1]
+print(sum(a[n])%1000000000)
